58d0844d30b6f58a55981fc4c0c26c2a22059f67
repl.it user <replituser@example.com>
2020-09-29 11:40:20 +0000

    Added folder called basics with two new python scripts

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,4 @@
+#prints system failure iminent and system reboot has been initated...
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+
---
+++ basics/output/simple_message.py
@@ -0,0 +1,2 @@
+#Code should print system failure iminent!
+print("system Failure Iminent!")
