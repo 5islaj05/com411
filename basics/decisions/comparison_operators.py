79c6fe8d50e4bb11cb6061248464f84692ed19af
repl.it user <replituser@example.com>
2020-10-06 10:31:29 +0000

    Added code to demonstrate comparison between two user inputs/variables

---
+++ basics/decisions/comparison_operators.py
@@ -0,0 +1,17 @@
+print("Please enter the first number.")
+firstNumber = int(input())
+print("Please enter the second number")
+secondNumber = int(input())
+
+if (firstNumber < secondNumber):
+  print("The first number is the smallest")
+
+elif (firstNumber > secondNumber):
+  print("The second number is the smallest")
+
+elif (firstNumber == secondNumber):
+  print("Both are equal")
+
+else:
+  print("Error")
+
