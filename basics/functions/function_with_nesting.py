ff7253d75c3517b2777ca9b4d84fc1759e472564
5islaj05 <5islaj05@solent.ac.uk>
2020-10-13 10:16:17 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,6 @@
+def identify():
+  liesBeyond = input("What lies beyond here? ")
+  if (liesBeyond == "a large boulder" or liesBeyond == "A large boulder"): 
+    print("It's time to run!")
+  else:
+    print("We'll be fine")
