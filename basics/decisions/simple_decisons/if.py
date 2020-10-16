e683bb336999cbd5f92f4e7c81b8e8a00788e7f8
repl.it user <replituser@example.com>
2020-10-16 23:57:08 +0000

    forgot to add this

---
+++ basics/decisions/simple_decisons/if.py
@@ -0,0 +1,5 @@
+User_Input = input("What type of book is this? ")
+if (User_Input == "adventure" or User_Input == "Adventure"):
+  print("I like adventure books!")
+  print("Finished reading book")
+  
