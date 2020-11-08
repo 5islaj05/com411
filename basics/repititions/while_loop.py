7c03ee34964b225a83741a12f25f97024dcde4d8
repl.it user <replituser@example.com>
2020-11-08 06:53:44 +0000

    Added code to demonstrate the use of a simple while loop

---
+++ basics/repititions/while_loop.py
@@ -0,0 +1,9 @@
+User_Input = int(input("How many cables should i remove? ")) #asks user the amount of cables to be removed
+
+iterations = 0 #sets up iteration counter
+while (User_Input > iterations):
+  print("Removed cable.") #announces cable being removed
+  iterations += 1 #adds one to the iterations
+
+  #keeps looping till iterations equals User_Input
+
