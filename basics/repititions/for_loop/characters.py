66f7cbd547f7335a784236a3ce72eb468c633405
repl.it user <replituser@example.com>
2020-11-09 10:51:56 +0000

    removed git

--- basics/repititions/for_loop/characters.py
+++ basics/repititions/for_loop/characters.py
@@ -6,6 +6,5 @@ print("Identifying..\n") #lets the user know the markings are being identified
 for i in range(0, len(marking), 1):
    #loops through the characters in the markings and individually print them out
     print("index", i, ":", marking[i])
-git
 print("Done !")
   
